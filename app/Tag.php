9c634ff8ed9a3da8c9ee808a2b7dfbb57db1b39f
I.Bozhkov <i.bozhkov@up2.nl>
2020-02-23 15:33:26 +0200

    Add Tags

---
+++ app/Tag.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+
+use Sqits\UserStamps\Concerns\HasUserStamps;
+
+class Tag extends Model
+{
+    use Translatable;
+    //   use HasUserStamps;
+
+    protected $guarded = [];
+    public $translatedAttributes = ['name'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+
+    }
+}
